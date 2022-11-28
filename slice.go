9642e1d178bf3a8e838daad5c56e3d28e8f4dfa2
Aksel Meola <aksel@meola.eu>
2022-11-28 20:13:46 +0200

    Add some generic slice utilities
     - SliceContainsElement  - alternative to ElementExists, but works with non []interface{} slices.
     - SliceContainsElementFunc - Check if element exists by condition
     - SliceFilterFunc - Filter slice by condition

--- slice.go
+++ slice.go
@@ -33,3 +33,37 @@ func UniqueElements(slice []interface{}) []interface{} {
 	}
 	return s
 }
+
+// SliceContainsElement returns true if the slice contains element el
+func SliceContainsElement[Element comparable](slice []Element, el Element) bool {
+	for _, v := range slice {
+		if v == el {
+			return true
+		}
+	}
+
+	return false
+}
+
+// SliceContainsElementFunc returns true if the comparisonFunc for an element returns true
+func SliceContainsElementFunc[Element comparable](slice []Element, comparisonFunc func(idx int, el Element) bool) bool {
+	for idx, v := range slice {
+		if comparisonFunc(idx, v) {
+			return true
+		}
+	}
+
+	return false
+}
+
+// SliceFilterFunc returns new slice which pass the filterFunc
+func SliceFilterFunc[Element comparable](slice []Element, filterFunc func(el Element) bool) []Element {
+	filteredSlice := make([]Element, 0)
+	for _, v := range slice {
+		if filterFunc(v) {
+			filteredSlice = append(filteredSlice, v)
+		}
+	}
+
+	return filteredSlice
+}
--- slice_test.go
+++ slice_test.go
@@ -1,7 +1,9 @@
 package goutils
 
 import (
+	"github.com/stretchr/testify/assert"
 	"reflect"
+	"strings"
 	"testing"
 )
 
@@ -100,4 +102,147 @@ func TestUniqueElements(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestSliceHasElement(t *testing.T) {
+	t.Run("Test nil slice has no element", func(t *testing.T) {
+		assert.False(t, SliceContainsElement(nil, "none"))
+	})
+
+	t.Run("Test string slice has element", func(t *testing.T) {
+		slice := []string{"foo", "bar", "baz"}
+		assert.True(t, SliceContainsElement(slice, "bar"))
+		assert.True(t, SliceContainsElement(slice, "foo"))
+		assert.False(t, SliceContainsElement(slice, "none"))
+		assert.False(t, SliceContainsElement(nil, "none"))
+	})
+
+	t.Run("Test int slice has element", func(t *testing.T) {
+		slice := []int{1, 2, 3, 4, 5}
+		assert.True(t, SliceContainsElement(slice, 1))
+		assert.True(t, SliceContainsElement(slice, 2))
+		assert.False(t, SliceContainsElement(slice, 99))
+	})
+
+	t.Run("Test custom slice has element", func(t *testing.T) {
+		slice := []struct {
+			Name string
+		}{
+			{
+				Name: "foo",
+			},
+			{
+				Name: "bar",
+			},
+		}
+		assert.True(t, SliceContainsElement(slice, struct{ Name string }{Name: "foo"}))
+		assert.True(t, SliceContainsElement(slice, struct{ Name string }{Name: "bar"}))
+		assert.False(t, SliceContainsElement(slice, struct{ Name string }{Name: "baz"}))
+	})
+}
+
+func TestSliceHasElementFunc(t *testing.T) {
+	t.Run("Test nil slice has no element with func", func(t *testing.T) {
+		var nilSlice []string
+		assert.False(t, SliceContainsElementFunc(nilSlice, func(idx int, el string) bool {
+			return false
+		}))
+	})
+
+	t.Run("Test string slice has element with func", func(t *testing.T) {
+		slice := []string{"first", "second", "third"}
+
+		// Slice has element which contains "ir"
+		assert.True(t, SliceContainsElementFunc(slice, func(idx int, el string) bool {
+			return strings.Contains(el, "ir")
+		}))
+
+		// Slice is missing element which contains "foo"
+		assert.False(t, SliceContainsElementFunc(slice, func(idx int, el string) bool {
+			return strings.Contains(el, "foo")
+		}))
+	})
+
+	t.Run("Test custom slice has element with func", func(t *testing.T) {
+		type Person struct {
+			Name string
+			Age  int
+		}
+		slice := []Person{
+			{
+				Name: "Foo",
+				Age:  10,
+			},
+			{
+				Name: "Foo",
+				Age:  99,
+			},
+		}
+
+		// Slice has person with name "Foo" and age "99"
+		assert.True(t, SliceContainsElementFunc(slice, func(idx int, el Person) bool {
+			return el.Name == "Foo" && el.Age == 99
+		}))
+
+	})
+
+	t.Run("Test pointer slice has element with func", func(t *testing.T) {
+		type Person struct {
+			Name string
+			Age  int
+		}
+		slice := []*Person{
+			nil,
+			nil,
+			nil,
+			nil,
+			{
+				Name: "Foo",
+				Age:  10,
+			},
+		}
+
+		// Slice has person with name "Foo" and age "99"
+		assert.True(t, SliceContainsElementFunc(slice, func(idx int, el *Person) bool {
+			return el != nil && el.Name == "Foo"
+		}))
+	})
+}
+
+func TestSliceFilterFunc(t *testing.T) {
+	t.Run("Test filtering string slice with func", func(t *testing.T) {
+		slice := []string{"A-string", "B-string", "C-string"}
+
+		// Filter out everything not starting with "B"
+		newSlice := SliceFilterFunc(slice, func(el string) bool {
+			return strings.HasPrefix(el, "B")
+		})
+		if assert.Len(t, newSlice, 1) {
+			assert.Equal(t, "B-string", newSlice[0])
+		}
+	})
+
+	t.Run("Test filtering custom structs", func(t *testing.T) {
+		type Person struct {
+			Name string
+			Age  int
+		}
+		slice := []Person{
+			{
+				Name: "Foo",
+				Age:  10,
+			},
+			{
+				Name: "Foo",
+				Age:  99,
+			},
+		}
+		// Filter out all persons under age 51
+		newSlice := SliceFilterFunc(slice, func(el Person) bool {
+			return el.Age > 50
+		})
+		if assert.Len(t, newSlice, 1) {
+			assert.Equal(t, 99, newSlice[0].Age)
+		}
+	})
 }
